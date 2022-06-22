b54df3b0cc2c076a4efa445c560275695dfde701
Dmitriy Voytenko <mailvoytenko@gmail.com>
2022-06-22 13:16:21 +0300

    JRTB-2: added stub telegram bot to the project

--- src/main/java/com/github/gitvoytenko/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/gitvoytenko/jrtb/JavarushTelegramBotApplication.java
@@ -1,4 +1,4 @@
-package com.github.gitvoytenko.javarushtelegrambot;
+package com.github.gitvoytenko.jrtb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
